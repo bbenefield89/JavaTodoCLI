bc25f89b4f4be81f27476f7f6a4ba23e620df149
bbenefield89 <bsquared18@gmail.com>
2018-11-23 20:15:59 -0500

    NOW PROMPTS USER WITH INSTRUCTIONS ON AVAILABLE NEXT ACTIONS
    - src/com/bbenefield/Main.java

--- src/com/bbenefield/Main.java
+++ src/com/bbenefield/Main.java
@@ -1,7 +1,54 @@
 package com.bbenefield;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, world!");
+    public static void main(String[] args) throws Exception {
+        File todoFile = new File("./src/com/bbenefield/resources/todos.txt");
+        File tempFile = new File("./src/com/bbenefield/resources/temp.txt");
+        FileReader fr = new FileReader(todoFile);
+        BufferedReader br = new BufferedReader(fr);
+        Scanner cliArgumentParser = new Scanner(System.in);
+        String cliArgument;
+
+        do {
+            System.out.printf(
+                    "\nWelcome back, would you like to\n" +
+                    "1: See your list of tasks\n" +
+                    "2: Add a new task\n" +
+                    "3: Remove a task\n" +
+                    "Q: Quit\n"
+            );
+
+            cliArgument = cliArgumentParser.nextLine().toLowerCase();
+
+            switch (cliArgument) {
+                case "1":
+                    System.out.println("LIST ALL TASKS");
+                    break;
+
+                case "2":
+                    System.out.println("ADD NEW TASK");
+                    break;
+
+                case "3":
+                    System.out.println("DELETE A TASK");
+                    break;
+
+                case "Q":
+                    System.out.println("LIST ALL TASKS");
+                    break;
+
+                default:
+                    System.out.printf("\"%s\" is not a supported command.", cliArgument);
+            }
+
+            System.out.println(cliArgument);
+        }
+        while (!cliArgument.equals("q"))
+            ;
     }
 }
